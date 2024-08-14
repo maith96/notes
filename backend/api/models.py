35aff41f431d6335985da622c8ce78bfbaba979e
maith96 <threedaniel385@gmail.com>
2024-08-14 13:58:29 +0300

    update api

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Note(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) -> str:
+        return self.title
