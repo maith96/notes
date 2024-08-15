abae6471a76e8e0b9390fafedd3e3333feffe42a
maith96 <threedaniel385@gmail.com>
2024-08-15 14:20:09 +0300

    add delete to url pattern

--- backend/items/urls.py
+++ backend/items/urls.py
@@ -10,11 +10,11 @@ from .views import (
 urlpatterns = [
     # SchoolID URLs
     path('school_ids/', SchoolIDListCreate.as_view(), name='school-id-list-create'),
-    path('school_ids/<int:pk>/', SchoolIDDelete.as_view(), name='school-id-delete'),
+    path('school_ids/delete/<int:pk>/', SchoolIDDelete.as_view(), name='school-id-delete'),
 
     # NationalID URLs
     path('national_ids/', NationalIDListCreate.as_view(), name='national-id-list-create'),
-    path('national_ids/<int:pk>/', NationalIDDelete.as_view(), name='national-id-delete'),
+    path('national_ids/delete/<int:pk>/', NationalIDDelete.as_view(), name='national-id-delete'),
 
     # Search URL
     path('search/<str:item_type>/<str:identifier>/', SearchItemByIdentifier.as_view(), name='search-item-by-identifier'),
