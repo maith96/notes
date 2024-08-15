ab08826219e37efac85ecf995f468367138cd31a
maith96 <threedaniel385@gmail.com>
2024-08-15 14:18:05 +0300

    added urlpatterns

---
+++ backend/items/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import (
+    SchoolIDListCreate,
+    SchoolIDDelete,
+    NationalIDListCreate,
+    NationalIDDelete,
+    SearchItemByIdentifier
+)
+
+urlpatterns = [
+    # SchoolID URLs
+    path('school_ids/', SchoolIDListCreate.as_view(), name='school-id-list-create'),
+    path('school_ids/<int:pk>/', SchoolIDDelete.as_view(), name='school-id-delete'),
+
+    # NationalID URLs
+    path('national_ids/', NationalIDListCreate.as_view(), name='national-id-list-create'),
+    path('national_ids/<int:pk>/', NationalIDDelete.as_view(), name='national-id-delete'),
+
+    # Search URL
+    path('search/<str:item_type>/<str:identifier>/', SearchItemByIdentifier.as_view(), name='search-item-by-identifier'),
+]
